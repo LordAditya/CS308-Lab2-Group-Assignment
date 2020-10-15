57a0604c0345324140879738b12c6f73d1f8fbb5
LordAditya <adibpandey@gmail.com>
2020-10-15 13:13:43 +0530

    Added file vecadd.c

---
+++ vecadd.c
@@ -0,0 +1,12 @@
+/* vecadd.c */
+// Each function in a separate file
+void add(vector vec_a, vector vec_b, vector *vec_c) {
+	// write code here to add vec_a and vec_b and 
+	// return the sum in vec_c
+	for(int j = 0 ; j < DIM ; j++){
+		*vec_c[j] = vec_a[j]+vec_b[j];
+	}
+}
+//////////////////////////////////////////////////////////
+
+
