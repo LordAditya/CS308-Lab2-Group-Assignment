647e1472d818068cb3b89bdc2b35605d2b0379b0
DNOrca <b18052@students.iitmandi.ac.in>
2020-10-16 12:17:20 +0530

    modified:   vecmain.c 	Slightly changed vecmain to get proper outputs of some functions 	And get better presentation of outputs.

--- vecmain.c
+++ vecmain.c
@@ -23,28 +23,29 @@ int main() {
 	for(int j = 0 ; j < DIM ; j++){
 		scanf("%f", &vecB[j]);
 	}
-  
+	
+	printf("Vector A:\n");
 	print(vecA);
+	printf("Vector B:\n");
 	print(vecB);
 	
-	// helloworld();
-	//printf("Printing vector sum\n");
-	//add(vecA,vecB,&vecC);
-	//print(&vecC);
+	printf("Printing vector sum, VecA + VecB: \n");
+	add(vecA,vecB,vecC);
+	print(vecC);
 
-	//printf("Printing vector-element wise product, vecA*vecB\n");
-	//eleProd(vecA,vecB,&vecC);
-	//print(&vecC);
+	printf("Printing vector-element wise product, vecA*vecB\n");
+	eleProd(vecA,vecB,vecC);
+	print(vecC);
 	
-	//printf("Printing vector-difference, vecA-vecB\n");
-	//diff(vecA, vecB, &vecC);
-	//print(vecC);
+	printf("Printing vector-difference, vecA-vecB\n");
+	diff(vecA, vecB, vecC);
+	print(vecC);
 
-	//printf("Printing dot-product, vecA.vecB\n");
-	//printf("%f",dotprod(vecA, vecB));
+	printf("Printing dot-product, vecA.vecB:\n");
+	printf("%f\n\n",dotprod(vecA, vecB));
 
-	printf("Printing angle between vecA and vecB, vecA-vecB\n");
-	printf("%f",vec_angle(vecA, vecB));
+	printf("Printing angle between vecA and vecB: \n");
+	printf("%f\n",vec_angle(vecA, vecB));
 
 	// print and display other outputs
 	// product, difference, dot product, angle
